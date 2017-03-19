f9280a7f940a704942659ff83dbede62aca50d45
lluz <luisluz@gmail.com>
2017-03-19 02:57:14 +0000

    work in progress

--- dist/js/jquery.jConveyorTicker.js
+++ dist/js/jquery.jConveyorTicker.js
@@ -22,20 +22,102 @@
             return false;
         }
 
+        var cycle_duration;
+        var reverse_commute;
+        var settings = {
+            anim_duration: 150,
+            reverse_elm: false
+        };
         
         if (options) {
-            
+            if ( options.anim_duration === undefined ) {
+                cycle_duration = settings.anim_duration;
+            }
+            else {
+                cycle_duration = options.anim_duration;
+            }
+
+            if ( options.reverse_elm === undefined ) {
+                reverse_commute = settings.reverse_elm;
+            }
+            else {
+                reverse_commute = options.reverse_elm;
+            }
+
+            $.extend(settings, options);
         }
 
         this.each(function(){
             var $wrap = $(this);
+            var $list = $wrap.children('ul');
+            $wrap.addClass('jctkr-wrapper');
+            if ( $list.width() >= ($list.parent().width() - 20) ){
+                var $listItemsClone = $list.clone().children('li');
+                $list.append($listItemsClone);
+                var listTotalWidth = 0;
+                $list.children().each(function(){
+                    listTotalWidth += $(this).outerWidth();
+                });
+                $list.width(listTotalWidth);
 
-            $wrap.addClass('jconvtick-wrapper');
+                var conveyorAnimate = function(action){
+                    var tw = $list.width();
+                    var tp = $list.position().left;
+                    var operator = '-';
+                    var direction = 'normal';
+                    var tcal;
+
+                    if (action !== undefined && action === 'reverse'){
+                        tcal = (tw / 2);
+                        if (tp > 0){
+                            $list.css('left', '-' + tcal + 'px');
+                            conveyorAnimate('reverse');
+                            return;
+                        }
+                        operator = '+';
+                        direction = 'reverse';
+                    }
+                    else {
+                        tcal = -1 * (tw / 2);
+                        if (tp < tcal){
+                            var toffs = -1 * (tcal - tp);
+                            $list.css('left', toffs + 'px');
+                            conveyorAnimate(direction);
+                            return;
+                        }
+                    }
+
+                    $list.animate({
+                        left: operator + '=10px'
+                    }, cycle_duration, 'linear', function(){
+                        conveyorAnimate(direction);
+                    });
+                };
+
+                $wrap
+                .hover(function(){
+                    $list.stop();
+                }, function(){
+                    $list.stop();
+                    conveyorAnimate('');
+                });
+
+                if ( reverse_commute ){
+                    $wrap.prev('.jctkr-label')
+                    .hover(function(){
+                        $list.stop();
+                        conveyorAnimate('reverse');
+                    }, function(){
+                        $list.stop();
+                        conveyorAnimate('normal');
+                    }).click(function(){
+                        return false;
+                    });
+                }
+                conveyorAnimate('normal');
+            }
+            $wrap.addClass('jctkr-initialized');
         });
-
-
-
-        /*************** */console.log('done');
     };
 
 })(jQuery, window, document);
